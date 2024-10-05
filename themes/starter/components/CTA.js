4e3714a1e83f3884621efa327a84d92bf873b4a8
sftbob <111048941+sftbob@users.noreply.github.com>
2024-10-05 19:00:20 +0800

    Update CTA.js

--- themes/starter/components/CTA.js
+++ themes/starter/components/CTA.js
@@ -12,26 +12,26 @@ export const CTA = () => {
   return (
     <>
       {/* <!-- ====== CTA Section Start --> */}
-      <section className='relative z-10 overflow-hidden bg-primary py-20 lg:py-[115px]'>
+      <section className='relative z-10 overflow-hidden bg-[#a61b29] py-20 lg:py-[115px]'>
         <div className='container mx-auto'>
           <div className='relative overflow-hidden'>
             <div className='-mx-4 flex flex-wrap items-stretch'>
               <div className='w-full px-4'>
                 <div className='mx-auto max-w-[570px] text-center'>
-                  <h2 className='mb-2.5 text-3xl font-bold text-white md:text-[38px] md:leading-[1.44]'>
+                  <h2 className='mb-2.5 text-3xl font-bold text-[#f1939c] md:text-[38px] md:leading-[1.44]'>
                     <span>{siteConfig('STARTER_CTA_TITLE')}</span>
                     <span className='text-3xl font-normal md:text-[40px]'>
                       {siteConfig('STARTER_CTA_TITLE_2')}
                     </span>
                   </h2>
-                  <p className='mx-auto mb-6 max-w-[515px] text-base leading-[1.5] text-white'>
+                  <p className='mx-auto mb-6 max-w-[515px] text-base leading-[1.5] text-[#f1939c]'>
                     {siteConfig('STARTER_CTA_DESCRIOTN')}
                   </p>
                   {siteConfig('STARTER_CTA_BUTTON') && (
                     <>
                       <a
                         href={siteConfig('STARTER_CTA_BUTTON_URL')}
-                        className='inline-block rounded-md border border-transparent bg-secondary px-7 py-3 text-base font-medium text-white transition hover:bg-[#0BB489]'>
+                        className='inline-block rounded-md border border-transparent bg-secondary px-7 py-3 text-base font-medium text-[#f1939c] transition hover:bg-[#a61b29]'>
                         {siteConfig('STARTER_CTA_BUTTON_TEXT')}
                       </a>
                     </>
